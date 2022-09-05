771738f0e0eeeaab320bf7dc6453b261d1be5bf9
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-09-05 15:29:05 +0200

    done

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,1 +1,26 @@
 ﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int n = Convert.ToInt32(Console.ReadLine());
+void PrintSumm(int m, int n, int summ)
+{
+    summ += n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма натуральных элементов в промежутке от первого числа до второго числа = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
+int summ = 0;
+int temp = m;
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSumm(m, n, summ);
