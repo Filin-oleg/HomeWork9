3da3e326429b41f3e45c2d141f3deea0847252d1
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-09-05 15:08:13 +0200

    done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int n = Convert.ToInt32(Console.ReadLine());
+void PrintNumber(int m, int n)
+{
+    if (m > n)
+    { return; }
+    else
+    {
+        Console.Write($"{m}, ");
+        PrintNumber(m + 1, n);
+    }
+}
+Console.WriteLine("Эта программа выводит все натуральные числа в промежутке от первого заданного Вами числа до второго заданного Вами числа");
+PrintNumber(m, n);
